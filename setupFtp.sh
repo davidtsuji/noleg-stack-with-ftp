3c3a1df9b5e54bd9d61fb9f7c1fa308c974e54b6
David Tsuji <david@flodge.com>
2014-10-25 11:07:01 +1000

    fixed some issues with the ftp setup

--- setupFtp.sh
+++ setupFtp.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+DOMAIN=%DOMAIN%
 FTP_USER=%FTP_USER%
 FTP_USER_PASSWORD=%FTP_USER_PASSWORD%
 
-sudo useradd coadd -g git -d /var/www/f4.flodge.com/current/public -s /bin/false
-echo -e "$FTP_USER_PASSWORD\$FTP_USER_PASSWORD" | (passwd --stdin $FTP_USER)
+sudo useradd $FTP_USER -g git -d /var/www/$DOMAIN/current/public -s /bin/false
+echo "$FTP_USER:$FTP_USER_PASSWORD" | chpasswd
 
 echo "Opening proftpd config"
 echo "----------------------"
 echo ""
-echo "1. Set `DefaultRoot ~`"
-echo "2. Set `RequireValidShell off`"
+echo "1. Set DefaultRoot ~"
+echo "2. Set RequireValidShell off"
 echo ""
 
 sudo vi /etc/proftpd/proftpd.conf
