16812ed10ab124eaa3b06b50d90a76a040a24511
David Tsuji <david@flodge.com>
2014-10-25 10:59:21 +1000

    updated setupFtp

--- setupFtp.sh
+++ setupFtp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+FTP_USER=%FTP_USER%
+FTP_USER_PASSWORD=%FTP_USER_PASSWORD%
+
 sudo useradd coadd -g git -d /var/www/f4.flodge.com/current/public -s /bin/false
+echo -e "$FTP_USER_PASSWORD\$FTP_USER_PASSWORD" | (passwd --stdin $FTP_USER)
 
 echo "Opening proftpd config"
 echo "----------------------"
